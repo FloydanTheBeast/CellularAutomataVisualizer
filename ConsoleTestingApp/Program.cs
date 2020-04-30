8815c5e718712730d97323c3d75a8655d14d974a
Shadi Abdelsalam <floydanthebeast@gmail.com>
2020-04-30 22:14:58 +0300

    Add console app project for testing purposes

---
+++ ConsoleTestingApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using CellularAutomata;
+
+namespace ConsoleTestingApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cell[] startingField = CellListGenerator.OneDimDefaultCells(101);
+            startingField[49] = new Cell(true);
+
+            GameField gf = new GameField(startingField, new int[] {-1, 0 , 1});
+            
+            RuleSet ruleSet = new RuleSet(new Rule[]
+            {
+                new ExactPatternRule(new Cell(true), new Cell[]
+                {
+                    new Cell(true),
+                    new Cell(false),
+                    new Cell(false)
+                }), 
+                new ExactPatternRule(new Cell(true), new Cell[]
+                {
+                    new Cell(false),
+                    new Cell(true),
+                    new Cell(true)
+                }),
+                new ExactPatternRule(new Cell(true), new Cell[]
+                {
+                    new Cell(false),
+                    new Cell(true),
+                    new Cell(false)
+                }),
+                new ExactPatternRule(new Cell(true), new Cell[]
+                {
+                    new Cell(false),
+                    new Cell(false),
+                    new Cell(true)
+                }),
+                new ExactPatternRule(new Cell(false), new Cell[]
+                {
+                    new Cell(true),
+                    new Cell(true),
+                    new Cell(true)
+                }),
+                new ExactPatternRule(new Cell(false), new Cell[]
+                {
+                    new Cell(true),
+                    new Cell(true),
+                    new Cell(false)
+                }),
+                new ExactPatternRule(new Cell(false), new Cell[]
+                {
+                    new Cell(true),
+                    new Cell(false),
+                    new Cell(true)
+                }),
+                new ExactPatternRule(new Cell(false), new Cell[]
+                {
+                    new Cell(false),
+                    new Cell(false),
+                    new Cell(false)
+                }),
+            }, new Cell());
+
+            for (int i = 0; i < 50; i++)
+            {
+                gf.PrintToConsole();
+                gf.ChangeField(ruleSet);
+                Console.ReadKey();
+            }
+        }
+    }
+}
