7ec68330717270119c6bcae09aca20de062e1fcb
Shadi Abdelsalam <floydanthebeast@gmail.com>
2020-05-05 12:57:13 +0300

    Fixed incorrect index in GetNeighborhood method

--- CellularAutomata/GameField.cs
+++ CellularAutomata/GameField.cs
@@ -4,60 +4,73 @@ namespace CellularAutomata
 {
     public class GameField
     {
-        Cell[][] _cells;
+        public Cell[][] Cells;
         readonly int[][] _neighborhoodCoords;
         bool _isInfinite;
+        public Int64 CurrentGeneration = 1;
         
         public GameField(Cell[][] cells, int[][] neighborhoodCoords, bool isInfinite = false)
         {
-            _cells = cells;
+            Cells = cells;
             _neighborhoodCoords = neighborhoodCoords;
             _isInfinite = isInfinite;
         }
 
         Cell[][] CopyCells()
         {
-            Cell[][] copiedCells = new Cell[_cells.Length][];
+            Cell[][] copiedCells = new Cell[Cells.Length][];
 
-            for (int i = 0; i < _cells.Length; i++)
+            for (int i = 0; i < Cells.Length; i++)
             {
-                copiedCells[i] = new Cell[_cells[i].Length];
-                for (int j = 0; j < _cells[i].Length; j++)
-                    copiedCells[i][j] = _cells[i][j].Clone() as Cell;
+                copiedCells[i] = new Cell[Cells[i].Length];
+                for (int j = 0; j < Cells[i].Length; j++)
+                    copiedCells[i][j] = Cells[i][j].Clone() as Cell;
             }
 
             return copiedCells;
         }
 
+        /// <summary>
+        /// Changes field according to passed rule set
+        /// </summary>
+        /// <param name="ruleSet">Set of rules</param>
         public void ChangeField(RuleSet ruleSet)
         {
             Cell[][] nextState = CopyCells();
             
-            for (int i = 0; i < _cells.Length; i++)
-                for (int j = 0; j < _cells[i].Length; j++)
+            for (int i = 0; i < Cells.Length; i++)
+                for (int j = 0; j < Cells[i].Length; j++)
                 {
                     Rule properRule = ruleSet.FindProperRule(GetNeighborhood(j, i));
                     properRule.Apply(ref nextState[i][j]);
                 }
 
-            _cells = nextState;
+            Cells = nextState;
+
+            // TODO: Catch overflow 
+            CurrentGeneration++;
         }
 
         Cell[] GetNeighborhood(int xCoordinate, int yCoordinate = 0)
         {
             Cell[] neighborhood = new Cell[_neighborhoodCoords.Length];
-            
+
             for (int i = 0; i < _neighborhoodCoords.Length; i++)
             {
                 // 1D automata - only one coordinate
                 try
                 {
                     if (_isInfinite)
-                        neighborhood[i] = _cells[(yCoordinate + (_neighborhoodCoords[i].Length >= 2 ? _neighborhoodCoords[i][1] : 0) + _cells.Length) % _cells.Length]
-                                                [(xCoordinate + _neighborhoodCoords[i][0] + _cells[i].Length) % _cells[i].Length];
+                    {
+                        neighborhood[i] = Cells[(yCoordinate + (_neighborhoodCoords[i].Length >= 2 ? _neighborhoodCoords[i][1] : 0) + Cells.Length) % Cells.Length]
+                                                [(xCoordinate + _neighborhoodCoords[i][0] + Cells[0].Length) % Cells[0].Length];
+                        }
                     else
-                        neighborhood[i] = _cells[yCoordinate + (_neighborhoodCoords[i].Length >= 2 ? _neighborhoodCoords[i][1] : 0)]
+                    {
+                        
+                        neighborhood[i] = Cells[yCoordinate + (_neighborhoodCoords[i].Length >= 2 ? _neighborhoodCoords[i][1] : 0)]
                                                 [xCoordinate + _neighborhoodCoords[i][0]];
+                    }
                 }
                 catch (IndexOutOfRangeException)
                 {
@@ -76,21 +89,21 @@ namespace CellularAutomata
 
         public void PrintToConsole()
         {
-            /* if _cells.Length is 1 then there's only one
+            /* if Cells.Length is 1 then there's only one
                 dimension otherwise there are two */
             
-            switch (_cells.Length)
+            switch (Cells.Length)
             {
                 case 1:
-                    foreach (Cell cell in _cells[0])
+                    foreach (Cell cell in Cells[0])
                         Console.Write(cell);
                     Console.WriteLine();
                     break;
                 default:
-                    for (int i = 0; i < _cells.Length; i++)
+                    for (int i = 0; i < Cells.Length; i++)
                     {
-                        for (int j = 0; j < _cells[i].Length; j++)
-                            Console.Write(_cells[i][j]);
+                        for (int j = 0; j < Cells[i].Length; j++)
+                            Console.Write(Cells[i][j]);
                         Console.WriteLine();
                     }
                     break;
